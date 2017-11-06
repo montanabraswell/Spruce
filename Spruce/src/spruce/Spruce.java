8486b1595bf8d9e4e111033ae8a44aab538a6ce8
montanabraswell <montanabraswell1@gmail.com>
2017-11-06 10:32:53 -0500

    Add files via upload
    Messing around with printing and tabing.

---
+++ Spruce/src/spruce/Spruce.java
@@ -0,0 +1,29 @@
+/* MOOC Java Excerise 3
+ * Java Tutorial 3
+ * Praticing println statements
+ * 10/26/17
+ */
+package spruce;
+
+/**
+ *
+ * @author Montana
+ */
+public class Spruce {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(" \t   * ");
+        System.out.print(" \t  *** ");
+        System.out.println(" \n \t *****");
+        System.out.println("\t******* ");
+        System.out.println("\t********");
+        System.out.println("\t   *");
+       // System.out.println(" \n\t* \n\t***\n\t****\n\t*****\n\t*******\n\t********\n\t*");//\n*****,\n\t *******,\n\t ********");
+        //System.out.println("\n\t ****");
+    }
+    
+}
